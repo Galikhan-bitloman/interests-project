a6a3da9394ca2eb63dfa33d52be38417bb606f42
galikhan <galihan.abdikarim@gmail.com>
2024-04-30 17:19:12 +0500

    check err nil

--- handlers/salary_calculate_handler.go
+++ handlers/salary_calculate_handler.go
@@ -1,22 +1,24 @@
 package handlers
 
 import (
-	"github.com/gofiber/fiber/v2"
 	"log"
 	"refactor/model"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func (h *Handler) SalaryCalculateHandler(ctx *fiber.Ctx) error {
 
 	s := new(model.SalaryRequest)
 	err := ctx.BodyParser(s)
-
 	if err != nil {
 		return err
 	}
 
 	data, err := h.services.SalaryCalculation.CalculateSalaryService(*s)
-
+	if err != nil {
+		return err
+	}
 	resp := *data
 
 	res := model.DataSalaryResponse{
